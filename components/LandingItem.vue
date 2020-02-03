488c28ba15a1a9127d198493c93ae685c38e87e0
Dries Bos <info@driesbos.com>
2020-02-03 10:51:05 +0000

    dynamic page transitions

--- components/LandingItem.vue
+++ components/LandingItem.vue
@@ -1,6 +1,9 @@
 <template>
+  <!-- prettier-ignore -->
   <section v-editable="blok" class="item landingItem">
-    <img :src="blok.image" alt="" />
+    <img :src="blok.image" alt />
+    <nuxt-link class="landingItem-Buttons prev button" to="/careers">careers</nuxt-link>
+    <nuxt-link class="landingItem-Buttons next button" to="/retail">retail</nuxt-link>
   </section>
 </template>
 
@@ -14,4 +17,20 @@ export default {
 
 <style lang="sass" scoped>
 .landingItem
+  position: relative
+  width: 100%
+  &-Buttons
+    position: absolute
+    bottom: 0
+    margin: var(--spacing-one)
+    &.prev
+      left: 0
+    &.next
+      right: 0
+.button
+  padding: var(--spacing-one) var(--spacing-two)
+  background: white
+  border-radius: 100px
+  border: 1px solid black
+  text-decoration: none
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <transition name="pages" mode="out-in">
+    <transition :name="transitionName" mode="out-in">
       <nuxt />
     </transition>
     <the-header />
@@ -13,6 +13,24 @@ import TheHeader from "~/components/TheHeader.vue"
 export default {
   components: {
     "the-header": TheHeader
-  }
+  },
+  data() {
+    return {
+      transitionName: "pages"
+    }
+  },
+  watch: {
+    $route(from, to) {
+      console.log(from, to)
+      if (from.path === "/careers" && to.path === "/") {
+        return (this.transitionName = "slideLeft")
+      } else if (from.path === "/retail" && to.path === "/") {
+        return (this.transitionName = "slideRight")
+      } else {
+        this.transitionName = "pages"
+      }
+    }
+  },
+  mounted() {}
 }
 </script>

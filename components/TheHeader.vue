20662d138bd62543d0d9ed87ac7695e5c08cf022
Dries Bos <info@driesbos.com>
2020-02-03 08:41:37 +0000

    header init

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,17 +1,35 @@
 <template>
-  <header>
-    <nav>
-      <ul>
-        <nuxt-link to="/" tag="li">Home</nuxt-link>
-        <nuxt-link to="/blog" tag="li">Blog</nuxt-link>
-        <nuxt-link to="/about" tag="li">About</nuxt-link>
-      </ul>
-    </nav>
+  <header class="header">
+    <transition name="fade">
+      <nav v-if="showHeader" class="header-Content" @click="toggleHeader">
+        <ul>
+          <nuxt-link to="/" tag="li">Home</nuxt-link>
+          <nuxt-link to="/brands" tag="li">Brands</nuxt-link>
+          <nuxt-link to="/about" tag="li">About</nuxt-link>
+          <nuxt-link to="/about" tag="li">News</nuxt-link>
+          <nuxt-link to="/about" tag="li">Retail</nuxt-link>
+          <nuxt-link to="/about" tag="li">Careers</nuxt-link>
+        </ul>
+      </nav>
+    </transition>
+    <div class="header-Toggle" @click="toggleHeader">
+      <p>toggle</p>
+    </div>
   </header>
 </template>
 
 <script>
 export default {
-  name: "TheHeader"
+  name: "TheHeader",
+  data() {
+    return {
+      showHeader: false
+    }
+  },
+  methods: {
+    toggleHeader() {
+      this.showHeader = !this.showHeader
+    }
+  }
 }
 </script>

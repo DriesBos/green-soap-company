a17852fd91e056ef7e47d8b38f6e5a9f8485b6d9
Dries Bos <info@driesbos.com>
2020-02-20 16:46:01 +0100

    style: update introItem

--- components/IntroductionItem.vue
+++ components/IntroductionItem.vue
@@ -7,7 +7,7 @@
       </div>
       <div class="introductionItem-Right">
         <div class="aspectRatioBox">
-          <img :src="blok.image" alt="" />
+          <img :src="blok.image" alt />
         </div>
       </div>
     </div>
@@ -21,39 +21,3 @@ export default {
   }
 }
 </script>
-
-<style lang="sass" scoped>
-.introductionItem
-  width: 100%
-  height: 100vh
-  display: flex
-  justify-content: center
-  align-items: center
-  &-Container
-    max-width: 1000px
-    display: flex
-    margin: var(--spacing-one)
-    > div
-      flex-basis: 50%
-  &-Left
-    display: flex
-    flex-direction: column
-    justify-content: center
-    h1, h2
-      padding-bottom: 1em
-    h1, h2, .markdown
-      margin-right: var(--spacing-one)
-  &-Right
-    .aspectRatioBox
-      position: relative
-      width: 100%
-      height: 0
-      padding-top: 100%
-      img
-        position: absolute
-        left: 0
-        top: 0
-        width: 100%
-        height: 100%
-        object-fit: cover
-</style>

55c5b4fe14fc862e0fd5496943b25c1760bbfa76
Dries Bos <info@driesbos.com>
2020-02-24 22:25:59 +0100

    init: careers page

---
+++ pages/careers/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="view-Careers">
+    <!-- <component
+      :is="story.content.component | dashify"
+      v-if="story.content.component"
+      :key="story.content._uid"
+      :blok="story.content"
+    ></component>-->
+  </div>
+</template>
+
+<script>
+import storyblokLivePreview from "@/mixins/storyblokLivePreview"
+
+export default {
+  components: {},
+  mixins: [storyblokLivePreview],
+  asyncData(context) {
+    return context.app.$storyapi
+      .get("cdn/stories/", {
+        version: "draft"
+      })
+      .then(res => {
+        return res.data
+      })
+      .catch(res => {
+        if (!res.response) {
+          console.error(res)
+          context.error({
+            statusCode: 404,
+            message: "Failed to receive content form api"
+          })
+        } else {
+          console.error(res.response.data)
+          context.error({
+            statusCode: res.response.status,
+            message: res.response.data
+          })
+        }
+      })
+  },
+  data() {
+    return {
+      stories: { content: {} }
+    }
+  },
+  mounted() {
+    console.log(this.stories)
+  }
+}
+</script>

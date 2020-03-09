28b9536a621b20766f84cb54ad2af4eeb796c043
Dries Bos <info@driesbos.com>
2020-03-09 13:46:27 +0100

    style: landingBlok icons

--- components/LandingItem.vue
+++ components/LandingItem.vue
@@ -2,8 +2,13 @@
   <!-- prettier-ignore -->
   <section v-editable="blok" class="item landingItem">
     <img :src="blok.image" alt />
-    <nuxt-link class="landingItem-Buttons prev button" to="/careers">careers</nuxt-link>
-    <nuxt-link class="landingItem-Buttons next button" to="/retail">retail</nuxt-link>
+    <nuxt-link class="landingItem-Buttons prev button" to="/careers">
+      <div class="icon arrow" v-html="require('~/assets/images/icon-arrow.svg?include')"></div>careers
+    </nuxt-link>
+    <nuxt-link class="landingItem-Buttons next button" to="/retail">
+      retail
+      <div class="icon arrow" v-html="require('~/assets/images/icon-arrow.svg?include')"></div>
+    </nuxt-link>
   </section>
 </template>
 

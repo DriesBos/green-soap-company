1ead44293ebdb1e0a5ddd1f65595c9f4a58aa527
Dries Bos <info@driesbos.com>
2020-03-31 22:00:19 +0200

    added textItem

---
+++ components/TextItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <section v-editable="blok" class="section textItem">
+    <div class="textItem-Wrapper">
+      <markdown :input="blok.title" />
+      <markdown :input="blok.text" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: Object
+  },
+  mounted() {
+    console.log(this.blok)
+  }
+}
+</script>
+
+<style lang="sass">
+@import '~/assets/styles/variables.sass'
+
+.textItem
+  width: 100%
+  display: flex
+  justify-content: center
+  &-Wrapper
+    width: 100%
+    max-width: $content-container
+  .markdown
+    width: 75%
+    p, h1, h2, h3, h4, h5
+      max-width: $max-text-width
+</style>
--- plugins/components.js
+++ plugins/components.js
@@ -6,6 +6,7 @@ import Page from "~/components/Page.vue"
 import LandingItem from "~/components/LandingItem.vue"
 import LandingSlider from "~/components/LandingSlider.vue"
 import IntroductionItem from "~/components/IntroductionItem.vue"
+import TextItem from "~/components/TextItem.vue"
 import ContentblokItem from "~/components/ContentblokItem.vue"
 import ContentblokWrapper from "~/components/ContentblokWrapper.vue"
 import VueCarouselItem from "~/components/VueCarouselItem.vue"
@@ -17,6 +18,7 @@ Vue.component("blok-page", Page)
 Vue.component("blok-landing-item", LandingItem)
 Vue.component("blok-landing-slider", LandingSlider)
 Vue.component("blok-introduction-item", IntroductionItem)
+Vue.component("blok-text-item", TextItem)
 Vue.component("blok-contentblok-item", ContentblokItem)
 Vue.component("blok-contentblok-wrapper", ContentblokWrapper)
 Vue.component("vue-carousel-item", VueCarouselItem)

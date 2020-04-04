9b5f79e7d60ee7527bb650dac528aadc41cc897c
Dries Bos <info@driesbos.com>
2020-04-04 13:06:30 +0200

    refactor: textItem to inhered cascaded styling

--- components/TextItem.vue
+++ components/TextItem.vue
@@ -1,7 +1,9 @@
 <template>
-  <section v-editable="blok" class="section textItem">
-    <div class="textItem-Wrapper">
-      <h2 v-if="blok.title" class="title">{{ blok.title }}</h2>
+  <section v-editable="blok" class="textItem contentContainer-Center">
+    <div class="textItem-Wrapper contentContainer-Center_Wrapper">
+      <div class="section-Header">
+        <h1 v-if="blok.title" class="title">{{ blok.title }}</h1>
+      </div>
       <markdown v-if="blok.text" :input="blok.text" />
     </div>
   </section>
@@ -17,21 +19,3 @@ export default {
   }
 }
 </script>
-
-<style lang="sass">
-@import '~/assets/styles/variables.sass'
-
-.textItem
-  width: 100%
-  display: flex
-  justify-content: center
-  &-Wrapper
-    width: 100%
-    max-width: $content-container
-  .title
-    font-size: 3.5556em
-    @media screen and ( min-width: $breakpoint-mobile)
-      margin-bottom: var(--spacing-three)
-    @media screen and ( max-width: $breakpoint-mobile)
-      margin-bottom: 1rem
-</style>

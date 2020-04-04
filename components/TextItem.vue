8b57b4d9f1d39b6f62978b4c365bb4795da9f5be
Dries Bos <info@driesbos.com>
2020-04-04 13:22:34 +0200

    fixes: paddings

--- components/TextItem.vue
+++ components/TextItem.vue
@@ -1,8 +1,8 @@
 <template>
   <section v-editable="blok" class="textItem contentContainer-Center">
     <div class="textItem-Wrapper contentContainer-Center_Wrapper">
-      <div class="section-Header">
-        <h1 v-if="blok.title" class="title">{{ blok.title }}</h1>
+      <div v-if="blok.title" class="section-Header">
+        <h1 class="title">{{ blok.title }}</h1>
       </div>
       <markdown v-if="blok.text" :input="blok.text" />
     </div>

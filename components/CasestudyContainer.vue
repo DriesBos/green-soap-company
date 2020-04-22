8526e290a0a109253d659e8778ef33af4592d0a0
Dries Bos <info@driesbos.com>
2020-04-22 12:28:34 +0200

    added logo to casestudyItem

--- components/CasestudyContainer.vue
+++ components/CasestudyContainer.vue
@@ -34,9 +34,54 @@ export default {
     flex-direction: column
   &-ContentWrapper
     display: flex
+    flex-wrap: wrap
     justify-content: space-between
     @media screen and ( max-width: $breakpoint-mobile)
       flex-direction: column
+  &-Item
+    position: relative
+    background: $color-blue
+    width: calc(50% - #{var(--spacing-two)})
+    height: 0
+    padding-top: 25%
+    margin-bottom: var(--spacing-three)
+    cursor: pointer
+    transition: background $transition-hover
+    @media screen and ( max-width: $breakpoint-mobile)
+      margin-bottom: var(--spacing-three)
+      width: 100%
+      padding-top: 75%
   &-Item:last-child
     margin-bottom: 0
+    padding-top: 35%
+    @media screen and ( max-width: $breakpoint-mobile)
+      padding-top: 75%
+  &-Content
+    position: absolute
+    display: flex
+    flex-direction: column
+    align-items: center
+    left: 50%
+    top: 50%
+    transform: translate(-50%, -50%)
+    text-align: center
+    h3
+      margin-bottom: var(--spacing-two)
+  &-Readmore
+    position: absolute
+    bottom: 1rem
+    left: 50%
+    transform: translateX(-50%)
+    text-transform: uppercase
+  &-Image
+    position: absolute
+    left: 50%
+    top: 50%
+    transform: translate(-50%, -50%)
+    width: 8rem
+    height: 8rem
+    img
+      width: 100%
+      height: 100%
+      object-fit: contain
 </style>
--- components/CasestudyItem.vue
+++ components/CasestudyItem.vue
@@ -2,9 +2,12 @@
   <!-- prettier-ignore -->
   <nuxt-link :to="blok.hyperlink.cached_url" tag="div" class="casestudy-Item readmore-Wrapper">
     <div class="casestudy-Content">
-      <h3 class="title">{{ blok.title }}</h3>
-      <p class="title readmore">Read More</p>
+      <!-- <h3 class="title">{{ blok.title }}</h3> -->
+      <div class="casestudy-Image">
+        <img :src="blok.logo" />
+      </div>
     </div>
+    <p class="title casestudy-Readmore">Read More</p>
   </nuxt-link>
 </template>
 
@@ -15,41 +18,3 @@ export default {
   }
 }
 </script>
-
-<style lang="sass">
-@import '~/assets/styles/variables.sass'
-
-.casestudy
-  &-Item
-    position: relative
-    background: $color-blue
-    flex-grow: 1
-    margin-right: var(--spacing-three)
-    height: 0
-    padding-top: 20%
-    cursor: pointer
-    transition: background $transition-hover
-    @media screen and ( max-width: $breakpoint-mobile)
-      margin-right: 0
-      margin-bottom: var(--spacing-three)
-      width: 100%
-      padding-top: 75%
-    &:last-child
-      margin-right: 0
-      padding-top: 30%
-      @media screen and ( max-width: $breakpoint-mobile)
-        padding-top: 75%
-    // &:hover
-    //   @media ( hover: hover )
-    //     background: $color-purple-hover
-  &-Content
-    position: absolute
-    left: 50%
-    top: 50%
-    transform: translate(-50%, -50%)
-    text-align: center
-    h3
-      margin-bottom: var(--spacing-two)
-    p
-      text-transform: uppercase
-</style>
